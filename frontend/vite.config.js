dcbe7b538f2ef7f7b72cef5d7cf0384bf9bf57ee
ntrngyn <krakenraz3@gmail.com>
2025-04-20 11:21:51 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 export default defineConfig({
-  base: "/A24132_NguyenTrongNguyen_TrangWebQuanLySach/",
+  base: "/B2207547_Nguyen_Trong_Nguyen_DO_AN_WEB/",
 
   plugins: [vue()],
   resolve: {
