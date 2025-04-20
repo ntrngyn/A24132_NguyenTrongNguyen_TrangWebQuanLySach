104dd1d2c0a2192bf837cd29030f53a60473de7a
ntrngyn <krakenraz3@gmail.com>
2025-04-20 10:50:28 +0700

    Initial commit: push to new repo

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  base: "/B2207547_Nguyen_Trong_Nguyen_DO_AN_WEB/",
+
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3001,
+  },
+
+  build: {
+    outDir: "../docs",
+  },
+});
